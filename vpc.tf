9c6caeaee1c31944fd6ed0ba0720f2e37e1ab530
Prashant Bhosale <bhoslepu@gmail.com>
2020-09-29 17:49:04 +0000

    Added VPC module

--- vpc.tf
+++ vpc.tf
@@ -2,7 +2,7 @@ module "vpc" {
   source  = "app.terraform.io/bhosaleprashant/vpc/aws"
   version = "2.55.0"
   # insert required variables here
-  cidr_block           = var.address_space
+  cidr           = var.address_space
   enable_dns_hostnames = true
 
   tags = {
